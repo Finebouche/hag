ab4b2629c85531ab07cb9a886da3d7c51fb1d398
Finebouche <tanguy.cazalets@protonmail.com>
2024-11-23 12:01:41 +0100

    Update richness visualization. Code to plot all function/dataset combination + typos + bugs

--- analysis/richness.py
+++ analysis/richness.py
@@ -61,7 +61,7 @@ def squared_uncoupled_dynamics(state_history, size_window=500, step_size=None, n
 
 
     # List to store the number of principal components for each window
-    num_components_list = []
+    evolution_num_components = []
 
     # Sliding window PCA
     for i in (tqdm(range(num_windows), desc="Squared Uncoupled Dynamics calculation") if show_progress else range(num_windows)):
@@ -85,12 +85,12 @@ def squared_uncoupled_dynamics(state_history, size_window=500, step_size=None, n
         num_components = np.argmax(cumulative_variance >= A) + 1
 
         # Append the result for this window
-        num_components_list.append(num_components)
+        evolution_num_components.append(num_components)
 
-    return num_components_list
+    return evolution_num_components
 
 # Should yield the same result as uncoupled_dynamics
-def squared_uncoupled_dynamics_alternative(state_history, size_window=1000, step_size=None, num_windows=None, theta=0.9, show_progress=True):
+def squared_uncoupled_dynamics_alternative(state_history, size_window=1000, step_size=None, num_windows=None, A=0.9, show_progress=True):
     state_history = np.array(state_history)
 
     number_of_steps = state_history.shape[0]
@@ -116,7 +116,7 @@ def squared_uncoupled_dynamics_alternative(state_history, size_window=1000, step
         window_data_std = (window_data - np.mean(window_data, axis=0)) / np.std(window_data, axis=0)
 
         # Compute the Singular Value Decomposition
-        S = np.linalg.svd(window_data, compute_uv=False)
+        S = np.linalg.svd(window_data_std, compute_uv=False)
 
         # Calculate the normalized relevance R_j
         R = S**2 / np.sum(S**2)
@@ -125,7 +125,7 @@ def squared_uncoupled_dynamics_alternative(state_history, size_window=1000, step
         cumulative_R = np.cumsum(R)
 
         # Find the minimum d such that the cumulative sum >= theta
-        num_components = np.searchsorted(cumulative_R, theta) + 1
+        num_components = np.searchsorted(cumulative_R, A) + 1
 
         evolution_num_components.append(num_components)
 
@@ -155,7 +155,6 @@ def linear_uncoupled_dynamics(state_history, size_window=1000, step_size=None, n
         start_index = i * step_size
         # Extract the current window of data
         window_data = state_history[start_index:start_index + size_window, :]
-        window_data_std = (window_data - np.mean(window_data, axis=0)) / np.std(window_data, axis=0)
 
         # Compute the Singular Value Decomposition
         S = np.linalg.svd(window_data, compute_uv=False)
@@ -201,7 +200,7 @@ def condition_number(state_history, size_window=1000, step_size=None, num_window
 
         # Compute the singular value decomposition and take the smallest and largest singular values
         S = np.linalg.svd(window_data, compute_uv=False)
-        condition_number = np.min(S) / np.max(S)
+        condition_number = np.max(S) / np.min(S)
         evolution_condition_number.append(condition_number)
 
     return evolution_condition_number
